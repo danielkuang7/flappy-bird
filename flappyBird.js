cf205c8a459424ac6c4d7c0d7b623499cbbab323
Daniel Kuang <danielkuang7@github.com>
2020-06-03 08:33:12 -0400

    Completed

--- flappyBird.js
+++ flappyBird.js
@@ -16,13 +16,51 @@ pipeNorth.src = "images/pipeNorth.png";
 pipeSouth.src = "images/pipeSouth.png";
 
 
+// some variables
+
+var gap = 85;
+var constant;
+
+var bX = 10;
+var bY = 150;
+
+var gravity = 1.5;
+
+var score = 0;
+
+// audio files
+
+var fly = new Audio();
+var scor = new Audio();
+
+fly.src = "sounds/fly.mp3";
+scor.src = "sounds/score.mp3";
+
+// on key down
+
+document.addEventListener("keydown",moveUp);
+
+function moveUp(){
+    bY -= 25;
+    fly.play();
+}
+
+// pipe coordinates
+
+var pipe = [];
+
+pipe[0] = {
+    x : cvs.width,
+    y : 0
+};
 
 // draw images
 
 function draw(){
     
     ctx.drawImage(bg,0,0);
-
+    
+    
     for(var i = 0; i < pipe.length; i++){
         
         constant = pipeNorth.height+gap;
@@ -38,13 +76,30 @@ function draw(){
             }); 
         }
 
-     // detect collision
+        // detect collision
         
-     if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
-        location.reload(); // reload the page
+        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
+            location.reload(); // reload the page
+        }
+        
+        if(pipe[i].x == 5){
+            score++;
+            scor.play();
+        }
+        
+        
     }
+
+    ctx.drawImage(fg,0,cvs.height - fg.height);
     
     ctx.drawImage(bird,bX,bY);
+    
+    bY += gravity;
+    
+    ctx.fillStyle = "#000";
+    ctx.font = "20px Verdana";
+    ctx.fillText("Score : "+score,10,cvs.height-20);
+    
     requestAnimationFrame(draw);
     
 }
